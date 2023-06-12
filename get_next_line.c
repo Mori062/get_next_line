d7e1ae9a2f4294e514464ac8b0a1093cdff05fa7
Mori062 <01morishi@gmail.com>
2023-06-12 10:46:27 +0900

    add main

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 00:04:36 by morishitash       #+#    #+#             */
-/*   Updated: 2023/06/11 01:38:36 by morishitash      ###   ########.fr       */
+/*   Updated: 2023/06/12 10:16:32 by morishitash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,45 @@
 
 char	*get_next_line(int fd)
 {
-	
+	char		*buff;
+	static char	*storage[OPEN_MAX + 1];
+	int			newline_pos;
+	int			bytes_read[1];
+
+	if ( fd < 0 ||  OPEN_MAX < fd )
+		return (NULL);
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	// 何かしらの処理
+	free(buff);
+	// 何かしらの処理
+	return ('なにかしらの文字列');
 }
+
+
+// #include <fcntl.h>
+// #include <stdio.h>
+// int main()
+// {
+//     char *test = "";
+//     int fd = open("test1.txt", O_RDONLY);
+//     test = get_next_line(fd);
+//     while (test)
+//     {
+//         printf("%s", test);
+//         free(test);
+//         test = get_next_line(fd);
+//     }
+//     printf("\n");
+//     free(test);
+//     close(fd);
+// }
+
+// __attribute__((destructor))
+// static void destructor() {
+//     system("leaks -q a.out");
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,20 @@
 /*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 00:05:03 by morishitash       #+#    #+#             */
-/*   Updated: 2023/06/11 01:38:30 by morishitash      ###   ########.fr       */
+/*   Updated: 2023/06/12 10:14:34 by morishitash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 1000
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
 
 char	*get_next_line(int fd);
 
