3a0d87b92b3947d7ea251ab26f71e8dfb26283bd
Mori062 <01morishi@gmail.com>
2023-06-13 19:57:13 +0900

    progress gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,52 +6,80 @@
 /*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 00:04:36 by morishitash       #+#    #+#             */
-/*   Updated: 2023/06/12 13:32:17 by morishitash      ###   ########.fr       */
+/*   Updated: 2023/06/13 19:45:00 by morishitash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	get_newline_pos(char *str)
+{
+	size_t	i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i + 1);
+		i++;
+	}
+	return (0);
+}
 
 char	*get_next_line(int fd)
 {
 	char		*buff;
 	static char	*storage[OPEN_MAX];
 	int			pos;
-	int			bytes_read[1];
 
-	if (fd < 0 || OPEN_MAX <= fd)
+	if (fd < 0 || OPEN_MAX <= fd || BUFFER_SIZE <= 0)
 		return (NULL);
 	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
 		return (NULL);
-	// 何かしらの処理
+	pos = get_newline_pos(storage[fd]);
+	while (pos == 0)
+	{
+		pos = read(fd, buff, BUFFER_SIZE);
+		if (pos == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[pos] = '\0';
+		storage[fd] = ft_strjoin(storage[fd], buff);
+		pos = get_newline_pos(storage[fd]);
+	}
 	free(buff);
-	// 何かしらの処理
-	return ('なにかしらの文字列');
+	return (ft_substr(storage[fd], 0, pos));
 }
 
 
-// #include <fcntl.h>
-// #include <stdio.h>
-// int	main(void)
-// {
-// 	char	*test = "";
-// 	int		fd;
 
-// 	fd = open("test1.txt", O_RDONLY);
-// 	test = get_next_line(fd);
-// 	while (test)
-// 	{
-// 		printf("%s", test);
-// 		free(test);
-// 		test = get_next_line(fd);
-// 	}
-// 	printf("\n");
-// 	free(test);
-// 	close(fd);
-// }
 
-// __attribute__((destructor)) static void	destructor(void)
-// {
-// 	system("leaks -q a.out");
-// }
+#include <fcntl.h>
+#include <stdio.h>
+int	main(void)
+{
+	char	*test = "";
+	int		fd;
+
+	fd = open("test1.txt", O_RDONLY);
+	test = get_next_line(fd);
+	while (test)
+	{
+		printf("%s", test);
+		free(test);
+		test = get_next_line(fd);
+	}
+	printf("\n");
+	free(test);
+	close(fd);
+}
+
+__attribute__((destructor)) static void	destructor(void)
+{
+	system("leaks -q a.out");
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 00:05:03 by morishitash       #+#    #+#             */
-/*   Updated: 2023/06/12 13:33:59 by morishitash      ###   ########.fr       */
+/*   Updated: 2023/06/13 19:56:38 by morishitash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,20 @@
 # include <limits.h>
 
 char	*get_next_line(int fd);
+
+
+
 char	*ft_strjoin(const char *s1, const char *s2);
+
+
+size_t	ft_strlen(const char *s);
+
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+
+char	*ft_strdup(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 #endif
